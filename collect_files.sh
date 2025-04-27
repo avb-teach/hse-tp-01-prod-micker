c0b03f4d24113084baabb689cf107b6fe35eb177
prod-micker <mickerthebeatmaker@gmail.com>
2025-04-27 19:23:24 +0300

    second version

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 INPUT="$1"
 OUTPUT="$2"
-FILES=$(find "$INPUT" -type f)
+DEPTH="$3"
+if [ -n "$DEPTH" ]; then
+  FILES=$(find "$INPUT" -maxdepth "$DEPTH" -type f)
+else
+  FILES=$(find "$INPUT" -type f)
+fi
 for FILE in $FILES; do
     cp "$FILE" "$OUTPUT"
 done
