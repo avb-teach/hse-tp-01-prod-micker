04e35038a7746547450f9e964612d3bca32ed70a
prod-micker <mickerthebeatmaker@gmail.com>
2025-04-29 00:39:01 +0300

    2nd опять version

--- collect_files.sh
+++ collect_files.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 INPUT="$1"
 OUTPUT="$2"
-DEPTH="$3"
-if [ -n "$DEPTH" ]; then # https://labex.io/ru/tutorials/shell-how-to-check-if-a-bash-variable-is-non-null-392930
-  FILES=$(find "$INPUT" -maxdepth "$DEPTH" -type f) # https://stackoverflow.com/questions/60089514/how-maxdepth-and-mindepth-works-with-find-command
-else
-  FILES=$(find "$INPUT" -type f)
-fi
+FILES=$(find "$INPUT" -type f)
 for FILE in $FILES; do
-  RPATH="${FILE#$INPUT/}" # https://stackoverflow.com/questions/16623835/remove-a-fixed-prefix-suffix-from-a-string-in-bash
-  mkdir -p "$(dirname "$OUTPUT/$RPATH")"
-  cp "$FILE" "$OUTPUT/$RPATH"
+    cp "$FILE" "$OUTPUT"
 done
