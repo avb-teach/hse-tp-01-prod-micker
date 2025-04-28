a1bad8deac07f30d680d36b05756067007a6165c
prod-micker <mickerthebeatmaker@gmail.com>
2025-04-29 00:34:46 +0300

    4th version

--- collect_files.sh
+++ collect_files.sh
@@ -8,7 +8,7 @@ else
   FILES=$(find "$INPUT" -type f)
 fi
 for FILE in $FILES; do
-  PATH="${FILE#$INPUT/}" # https://stackoverflow.com/questions/16623835/remove-a-fixed-prefix-suffix-from-a-string-in-bash
-  mkdir -p "$(dirname "$OUTPUT/$PATH")"
-  cp "$FILE" "$OUTPUT/$PATH"
+  RPATH="${FILE#$INPUT/}" # https://stackoverflow.com/questions/16623835/remove-a-fixed-prefix-suffix-from-a-string-in-bash
+  mkdir -p "$(dirname "$OUTPUT/$RPATH")"
+  cp "$FILE" "$OUTPUT/$RPATH"
 done
